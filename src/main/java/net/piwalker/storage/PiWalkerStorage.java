4173bee1af9ae19712bc7ce18f84fc5d4eff5924
Samuel Walker <ldspcjunkie@gmail.com>
2022-07-30 20:45:06 -0600

    Initial commit

---
+++ src/main/java/net/piwalker/storage/PiWalkerStorage.java
@@ -0,0 +1,17 @@
+package net.piwalker.storage;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PiWalkerStorage implements ModInitializer {
+	public static final String MOD_ID = "piwalker_storage";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Initializing mod " + MOD_ID);
+
+	}
+}
